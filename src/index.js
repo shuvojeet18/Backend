4107e17e652cb811bc20bea53af2c9c844092d37
Shuvojeet18 <shuvojeet623@gmail.com>
2025-09-26 00:57:37 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
    res.send("Hello World!");
 });
 
+app.get("/health", (req, res) => {
+   res.status(200).send("OK");
+});
+
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
 });
